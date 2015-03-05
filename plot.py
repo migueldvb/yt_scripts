bdc73fd528ba392a8cfcc307f7300d4be98c554e
Miguel de Val-Borro <miguel@archlinux.net>
2015-03-05 18:07:02 -0500

    convert kepler to roche problem

--- plot.py
+++ plot.py
@@ -4,14 +4,14 @@ import yt
 from os.path import join
 
 # Load the dataset.
-basedir = "/home/miguel/mnt/piernik/kepler"
-ds = yt.load(join(basedir, "kepler_tst_0020.h5"))
+basedir = "/home/miguel/mnt/piernik/runs/roche"
+ds = yt.load(join(basedir, "roche_tst_0020.h5"))
 
 # Create density slices in all three axes.
 print(ds.field_list)
 print(ds.domain_width)
 
 for i in range(33):
-    ds = yt.load(join(basedir, "kepler_tst_00{:02d}.h5".format(i)))
-    p = yt.SlicePlot(ds, "z", "deni")
+    ds = yt.load(join(basedir, "roche_tst_00{:02d}.h5".format(i)))
+    p = yt.SlicePlot(ds, "z", "denn")
     p.save()
