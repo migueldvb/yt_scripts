494809d5500a7011a849c5cebfe3e8ba18da5cbb
Miguel de Val-Borro <miguel@archlinux.net>
2015-03-11 16:51:27 -0400

    update variable name

--- piernik/plot.py
+++ piernik/plot.py
@@ -8,15 +8,15 @@ import glob
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', help='data directory',
     default="/home/miguel/project/hydro/runs/piernik/")
-args = parser.parse_args(
+args = parser.parse_args()
 
 # Load the dataset.
 # ds = yt.load(join(basedir, "roche_tst_0020.h5"))
 # Create density slices in all three axes.
-print(ds.field_list)
-print(ds.domain_width)
+# print(ds.field_list)
+# print(ds.domain_width)
 
 for i in glob.glob(join(args.dir, '*.h5')):
     ds = yt.load(i)
-    p = yt.SlicePlot(ds, "z", "denn")
+    p = yt.SlicePlot(ds, "z", "deni")
     p.save()
