5cf4b51f3aa11174733aa3c1594492025478da8b
Miguel de Val-Borro <miguel@archlinux.net>
2015-03-11 16:45:46 -0400

    python setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='yt_scripts',
+    description='yt scripts',
+    author='Miguel de Val-Borro',
+    author_email='miguel@archlinux.net',
+    version='0.1',
+    scripts=['piernik/plot.py',
+        ],
+    )
