a4eec9d3d4f134ab224276a136df41943fe28809
Miguel de Val-Borro <miguel@archlinux.net>
2015-03-11 21:56:10 -0400

    print dataset info

--- piernik/plot.py
+++ piernik/plot.py
@@ -13,10 +13,10 @@ args = parser.parse_args()
 # Load the dataset.
 # ds = yt.load(join(basedir, "roche_tst_0020.h5"))
 # Create density slices in all three axes.
-# print(ds.field_list)
-# print(ds.domain_width)
 
 for i in glob.glob(join(args.dir, '*.h5')):
     ds = yt.load(i)
+    print(ds.field_list)
+    print(ds.domain_width)
     p = yt.SlicePlot(ds, "z", "deni")
     p.save()
