c27b9c7f9d06e76fe3cbc8fe369e908da95ec6c1
Miguel de Val-Borro <miguel@archlinux.net>
2015-03-17 11:00:51 -0400

    add dens field for FLASH test problem

--- piernik/plot.py
+++ piernik/plot.py
@@ -11,7 +11,7 @@ parser.add_argument('--dir', help='data directory',
 parser.add_argument('-f', '--filename', help='HDF5 file', default="")
 parser.add_argument('-a', '--axis', type=int, help='axis', default=2)
 parser.add_argument('--field', help='field variable', default="deni",
-        choices=('deni', 'vlxi', 'vlyi', 'vlzi'))
+        choices=('dens', 'deni', 'vlxi', 'vlyi', 'vlzi'))
 args = parser.parse_args()
 
 # Load the dataset.
@@ -32,7 +32,7 @@ def plot_h5(filename):
     print(ds.domain_width)
     p = yt.SlicePlot(ds, args.axis, args.field,
             axes_unit='au',
-#             center=([0.5, 0.5, 0.], 'unitary'),
+#             center=([0.5, 0.5, 0.5], 'unitary'),
             origin="native")
     p.save()
 
